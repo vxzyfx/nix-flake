b2e3fe091437d0c879ae4aa79747001e6b76a095
shug <vxzyfx@gmail.com>
2024-11-19 20:07:21 +0800

    feat: linux alt space launch app

--- home/linux/hyprland.nix
+++ home/linux/hyprland.nix
@@ -128,6 +128,7 @@
           ", XF86AudioRaiseVolume, exec, uwsm app -- wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
           ", XF86AudioLowerVolume, exec, uwsm app -- wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-"
           "$mod, R, exec, uwsm app -- fuzzel"
+          "ALT, SPACE, exec, uwsm app -- fuzzel"
           "$mod, return, exec, uwsm app -- kitty"
         ]
         ++ (
