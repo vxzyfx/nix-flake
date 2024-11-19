a39c3459e6802889b5ebe8db6ff8f7940b6d5ca3
vxshug <vxzyfx@gmail.com>
2024-11-19 11:19:30 +0800

    feat: vscode

--- hosts/darwin-shugMacStudio/home.nix
+++ hosts/darwin-shugMacStudio/home.nix
@@ -1,3 +1,14 @@
+{...}:
 {
-  
+  programs.starship = {
+    enableZshIntegration = true;
+    enable = true;
+    settings = {
+      add_newline = false;
+      command_timeout = 1000;
+      battery = {
+        disabled=true;
+      };
+    };
+  };
 }
